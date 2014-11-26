3c45f2323cf3bbc999fbd73e45453dc0f4423121
tlichten <tlichten@netapp.com>
2014-11-26 16:53:39 +0100

    More conservative timing for cluster setup api calls, removed unsupported Vagrant ssh options

--- enablecluster.sh
+++ enablecluster.sh
@@ -2,11 +2,11 @@
 sleep 5
 echo "Trying VSIM cluster setup on $1"
 curl -X POST -d @cluster_setup "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
-sleep 15
+sleep 25
 curl -X POST -d @ssh_enable "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
-sleep 2
-curl -X POST -d @ssh_enable_publickey "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
 sleep 5
+curl -X POST -d @ssh_enable_publickey "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
+sleep 10
 echo "\n"
 echo "SSH is available at Cluster Node Management ip $1. Username admin, password netapp123"
 
