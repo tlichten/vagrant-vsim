c55e4f6da2dad330248bde352467981c3551837c
tlichten <tlichten@netapp.com>
2014-11-27 09:47:31 +0100

    Use no proxy when connecting to VSim

--- enablecluster.sh
+++ enablecluster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sleep 5
 echo "Trying VSIM cluster setup on $1"
-curl -X POST -d @cluster_setup "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
+curl -X POST -d @cluster_setup --noproxy $1 "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
 sleep 25
-curl -X POST -d @ssh_enable "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
+curl -X POST -d @ssh_enable --noproxy $1 "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
 sleep 5
-curl -X POST -d @ssh_enable_publickey "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
+curl -X POST -d @ssh_enable_publickey --noproxy $1 "http://admin@$1/servlets/netapp.servlets.admin.XMLrequest_filer"
 sleep 10
 echo "\n"
 echo "SSH is available at Cluster Node Management ip $1. Username admin, password netapp123"
