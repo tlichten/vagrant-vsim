51be15679d38c29072914534cbc427827975ee56
tlichten <tlichten@netapp.com>
2015-03-02 10:33:54 +0100

    Fixing here documents permissions

--- service.sh
+++ service.sh
@@ -3,11 +3,11 @@
 sudo apt-get -y update
 sudo apt-get -y install dnsmasq sshpass
 BASEIP=`echo $NODE_MGMT_IP | cut -d"." -f1-3`
-sudo cat <<EOF >> /etc/dnsmasq.conf
+sudo sh -c "cat <<EOF >> /etc/dnsmasq.conf
 dhcp-range=$BASEIP.60,$BASEIP.62,12h
 dhcp-host=08:00:DE:AD:AC:1D,vsim,$NODE_MGMT_IP,infinite
 log-dhcp
-EOF
+EOF"
 sudo /etc/init.d/dnsmasq restart
 
 sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 22222 -j REDIRECT --to-port 22
