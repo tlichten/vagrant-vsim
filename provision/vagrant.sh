7ae637072a21c5935a66d9ae917d2c96e3e6a998
tlichten <tlichten@netapp.com>
2015-03-03 14:25:47 +0100

    Fix spacing

--- provision/vagrant.sh
+++ provision/vagrant.sh
@@ -21,6 +21,6 @@ fi
 
 # start tapster provisioning
 echo "Start $PROVISION provisioning"
-PATH_PROVISION ="$TAPSTER/provision"
+PATH_PROVISION="$TAPSTER/provision"
 su $OS_USER -c "chmod +x $PATH_PROVISION/$PROVISION.sh"
 su $OS_USER -c "cd $PATH_PROVISION && ./$PROVISION.sh"
