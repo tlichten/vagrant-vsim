8db2d6decebbd2551c30c3ea6ed3ffd9ec3515b8
tlichten <tlichten@netapp.com>
2015-06-20 18:36:14 +0200

    Disabling cluster setup etc. on second node

--- provision/vsim2.sh
+++ provision/vsim2.sh
@@ -120,44 +120,6 @@ then
   API_ENDPOINT=$API_ENDPOINT_INIT
 fi
 sleep 5
-echo "Starting cluster setup on $NODE_MGMT_IP"
-/usr/bin/curl -X POST -d "$CLUSTER_SETUP"  -sS --noproxy $NODE_MGMT_IP $API_ENDPOINT
-sleep 60
-echo "Enabling SSH access for $CLUSTER_USERNAME"
-/usr/bin/curl -X POST -d "$SSH_ENABLE"  -sS --noproxy $NODE_MGMT_IP $API_ENDPOINT
-sleep 10
-echo "Enabling SSH public key auth"
-/usr/bin/curl -X POST -d "$SSH_ENABLE_PUBLICKEY" -sS --noproxy $NODE_MGMT_IP $API_ENDPOINT
-sleep 5
-echo "Assigning disks"
-/usr/bin/curl -X POST -d "$DISK_ASSIGN" -sS --noproxy $NODE_MGMT_IP $API_ENDPOINT
-sleep 25
-
-if [ -n "$LICENSES" ]; then
-  echo "Adding additional licenses $LICENSES"
-  /usr/bin/curl -X POST -d "$ADD_LICENSES" -sS --noproxy $NODE_MGMT_IP $API_ENDPOINT
-  sleep 5
-fi
-
-echo "Enabling Ontapi access for admin user $CLUSTER_USERNAME"
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $CLUSTER_USERNAME@$NODE_MGMT_IP -t "security login create -username $CLUSTER_USERNAME -application ontapi -authmethod password -role admin" 2>/dev/null
-
-echo "Adding public key for $CLUSTER_USERNAME"
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $CLUSTER_USERNAME@$NODE_MGMT_IP -t 'security login publickey create -username vagrant -index 0 -publickey "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ=="' 2>/dev/null
-
-
-echo "Running chef recipes"
-cd /home/vagrant/chef && chef-solo -c solo.rb -j web.json
-
-
-echo "Running additional commands"
-while read -u 3 p; do
-  [[ -z "$p" ]] && continue
-  echo $p
-  [[ "$p" =~ ^#.*$ ]] && continue
-  sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $CLUSTER_USERNAME@$NODE_MGMT_IP -t "$p" 2>/dev/null
-  sleep 15
-done 3</vagrant/vsim.cmds
 
 
 echo "SSH is available at $NODE_MGMT_IP. username $CLUSTER_USERNAME, password $PASSWORD"
